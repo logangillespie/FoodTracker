5bd8d556c6c8b2d1172403b45d451978342370ef
Logan Gillespie <logangillespie@logans-macbook-pro-2.local>
2020-06-22 15:01:28 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Logan Gillespie on 6/22/20.
+//  Copyright © 2020 Logan Gillespie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
